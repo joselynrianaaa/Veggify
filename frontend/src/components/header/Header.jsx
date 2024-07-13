fc24f0dd883d7cbae1d64fd3b67b8f2d0c84417c
joselynriana <joselynriana@gmail.com>
2024-07-14 00:08:57 +0530

    initial

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  const [hideLeft, setHideLeft] = useState("-left-[1000px]")
+  const menuItems = ["recipes" , "resources" , "about" , "contact"]
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
